895353a3eec6b14df7bb10eae0794c34973b4c98
Rafał Daniel Wolert <wolertr@gmail.com>
2020-12-18 14:39:29 +0100

    Utworzenie szablonów klas: StacjaRowerowa, SystemRowerowy.

--- system/src/StacjaRowerowa.java
+++ system/src/StacjaRowerowa.java
@@ -1,2 +1,65 @@
+import java.util.List;
+
+/** Klasa StacjaRowerowa ...
+ *
+ */
 public class StacjaRowerowa {
+    private List<Rower> Stojak;
+    private int x;
+    private int y;
+
+    /**
+     * @param Stojak Lista obiektów klasy Rower.
+     * @param x Pierwsza współrzędna obiektu klasy StacjaRowerowa.
+     * @param y Druga współrzędna obiektu klasy Stacjarowerowa.
+     */
+    public StacjaRowerowa (List<Rower> Stojak, int x, int y){
+        this.Stojak = Stojak;
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public List<Rower> getStojak() {
+        return Stojak;
+    }
+
+    public void setStojak(List<Rower> stojak) {
+        Stojak = stojak;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Metoda obliczająca liczbę rowerów w stojaku
+    public int liczbaRowerow() {
+        return -1;
+
+    }
+
+    // Metoda zwracająca obiekt klasy Stojak
+    public List<Rower> ktoryStojak(Object Rower) {
+        return Stojak;
+    }
+
+    // Metoda ,,przyjmująca'' rower do stacji rowerowej
+    public void przyjmijRower() {
+
+    }
+
+
+
 }
--- system/src/SystemRowerowy.java
+++ system/src/SystemRowerowy.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/** Klasa SystemRowerowy
+ *
+ */
 public class SystemRowerowy {
+    private List<Rower> listaRowerow;
+    private ArrayList<StacjaRowerowa> stacjeRowerowe;
+
+    /**
+     * @param listaRowerow wypełnić
+     * @param stacjeRowerowe wypełnić
+     */
+    public SystemRowerowy (ArrayList<Rower> listaRowerow, ArrayList<StacjaRowerowa> stacjeRowerowe) {
+        this.listaRowerow = listaRowerow;
+        this.stacjeRowerowe = stacjeRowerowe;
+    }
+
+
+//    public void StacjaRowerowa jakaStacja(List<Integer> lokalizacja) {
+//        // return stacjeRowerowe.get(lokalizacja.get());
+//    }
+
+    public Rower jakiRower(int nrRoweru) {
+        return listaRowerow.get(nrRoweru);
+    }
+    // Metoda do pobierania opłaty za konto.
+
+    public void pomniejszKonto(double czasWypozyczenia) {
+
+    }
+    // Metoda do zasilania konta.
+
+    public void powiekszKonto(double kwota) {
+
+    }
 }
